2e041999d332719d196475298905588e54b8ce0a
Igor <dorofeyev07@gmail.com>
2012-06-04 13:29:25 +0400

    Test commit.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,6 +5,7 @@ MainWindow::MainWindow( QWidget *parent )
     : QMainWindow( parent )
 {
   m_renderWidget = new RenderWidget( this );
+  m_renderWidget->showFPS( false );
   setCentralWidget( m_renderWidget );
 }
 
